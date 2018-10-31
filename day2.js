dbfb6782dfc2a6d8e450eb667e2be40ad621a3ee
meghanaram <ramachandra.meghana@gmail.com>
2018-10-31 15:41:17 +0530

    few changes has made

---
+++ day2.js
@@ -0,0 +1,9 @@
+Web Application Firewall:
+*Application firewall for http applications
+*It applies a set of rules to an http conversation
+*Generally these rules cover common attacks such as cross-site scripting, sql injection
+*WAF can be considered as reverse proxy.                                                                         
+Dedos attacks
+*Malicious attempt to disrupt normal traffic of a targeted server by overwhelming the target or its surrounding infrastructure with a flood of internet traffic.
+Proxy servers
+*Communication between two computers connected through a third computer acting as a proxy
