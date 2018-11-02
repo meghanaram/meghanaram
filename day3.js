7d999b4596ff0386fdb900e279135c03331c81d6
meghanaram <ramachandra.meghana@gmail.com>
2018-11-02 17:07:30 +0530

    day3.js is added

---
+++ day3.js
@@ -0,0 +1,3 @@
+Penetration Testing: A Penetration test, also known as pen test, is a simulated cyberattack against your computer system to check for exploitable vulnerabilities.
+In the context of web application security, penetration testing is commonly used to augment a WAF.
+
